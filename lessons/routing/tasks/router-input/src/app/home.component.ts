4c0cb123df3f6029be5a04e13913bf3ae4f084a1
Plotnikova Darya <dvikhlyantseva@embedika.ru>
2026-09-02 10:58:36 +0500

    Add Routing task

--- lessons/routing/tasks/router-input/src/app/home.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-@Component({
-  selector: 'app-home',
-  imports: [],
-  template: `
-    <div>Home</div>
-  `,
-})
-export default class HomeComponent {}
